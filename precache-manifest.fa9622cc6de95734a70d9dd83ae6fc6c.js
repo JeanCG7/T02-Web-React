8e6af9e7b9686f2949173e5c31a5282b4db6ae1b
jeancg7 <jean.goncalves7@etec.sp.gov.br>
2018-11-08 14:00:44 -0200

    Updates

---
+++ precache-manifest.fa9622cc6de95734a70d9dd83ae6fc6c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c0e654e03d16f28a6b0536877b4a515",
+    "url": "/T02-Web-React/static/media/reactBig.8c0e654e.png"
+  },
+  {
+    "revision": "102a346cab4ba2c6bd99",
+    "url": "/T02-Web-React/static/css/main.420abd1b.chunk.css"
+  },
+  {
+    "revision": "1f817fc0ba24b22a25a8",
+    "url": "/T02-Web-React/static/js/1.1f817fc0.chunk.js"
+  },
+  {
+    "revision": "824300bcc7b782b1dc03",
+    "url": "/T02-Web-React/static/js/runtime~main.824300bc.js"
+  },
+  {
+    "revision": "866e631ea2b571c3c8757c49562eb33f",
+    "url": "/T02-Web-React/static/media/eu.866e631e.jpg"
+  },
+  {
+    "revision": "69273c1281c8bab5fceee4583d109994",
+    "url": "/T02-Web-React/static/media/jeanCurriculo.69273c12.pdf"
+  },
+  {
+    "revision": "a7c3b2a902961daa803c1b0e57f22b48",
+    "url": "/T02-Web-React/static/media/blog1.a7c3b2a9.png"
+  },
+  {
+    "revision": "102a346cab4ba2c6bd99",
+    "url": "/T02-Web-React/static/js/main.102a346c.chunk.js"
+  },
+  {
+    "revision": "2fd74ca04f381438f3dc9aa4fff0f9d4",
+    "url": "/T02-Web-React/static/media/cool.2fd74ca0.jpg"
+  },
+  {
+    "revision": "4a15d6e7a97f29452e2e6584cb809a97",
+    "url": "/T02-Web-React/static/media/todo.4a15d6e7.jpg"
+  },
+  {
+    "revision": "617f1f47ccfd26581e889d2d7ae37dfc",
+    "url": "/T02-Web-React/static/media/reactProject.617f1f47.jpg"
+  },
+  {
+    "revision": "72a3588015129e83a1f29042c2e9b690",
+    "url": "/T02-Web-React/static/media/utfpr.72a35880.png"
+  },
+  {
+    "revision": "73216bf6a8b9a313e12985cf97e0c485",
+    "url": "/T02-Web-React/static/media/progress.73216bf6.png"
+  },
+  {
+    "revision": "a7a9e09d9c93782876dd10c94999990d",
+    "url": "/T02-Web-React/static/media/taruma.a7a9e09d.jpg"
+  },
+  {
+    "revision": "ceb633a24c2e6f1cea6fbda9c92ab680",
+    "url": "/T02-Web-React/index.html"
+  }
+];
